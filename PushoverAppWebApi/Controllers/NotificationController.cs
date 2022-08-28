83d4e0a17ab947368281fa837e96ce18e16e78c5
Andrey <morgunof-andrey@yandex.ru>
2022-08-28 15:35:46 +0300

    Add notification controller

--- PushoverAppWebApi/Controllers/NotificationController.cs
+++ PushoverAppWebApi/Controllers/NotificationController.cs
@@ -6,16 +6,25 @@ namespace PushoverAppWebApi.Controllers;
 [Route("[controller]")]
 public class NotificationController : ControllerBase
 {
+    private readonly IHttpClientFactory _httpClientFactory;
     private readonly ILogger<NotificationController> _logger;
 
-    public NotificationController(ILogger<NotificationController> logger)
+    public NotificationController(ILogger<NotificationController> logger, IHttpClientFactory httpClientFactory)
     {
         _logger = logger;
+        _httpClientFactory = httpClientFactory;
     }
 
-    [HttpPost(Name = "PostNotification")]
-    public IEnumerable<string> Post()
+    [HttpPost]
+    public async Task<IResult> Post(string tokenKey, string userKey, string message)
     {
-        throw new NotImplementedException();
+        var httpRequestMessage = new HttpRequestMessage(
+            HttpMethod.Post,
+            $"https://api.pushover.net/1/messages.json?token={tokenKey}&user={userKey}&message={message}");
+
+        var httpClient = _httpClientFactory.CreateClient();
+        var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+        return httpResponseMessage.IsSuccessStatusCode ? Results.Ok() : Results.BadRequest();
     }
 }
